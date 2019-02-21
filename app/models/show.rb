4f587e2d9cb361d684d121553738d4337125cf6c
Sara Walker <sarawall1187@gmail.com>
2019-02-21 22:53:17 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,14 @@
 class Show < ActiveRecord::Base
+
+  def Show::highest_rating
+    Show.maximum(:rating)
+  end
+
+  def Show::most_popular_show
+    Show.where(highest_rating).first
+  end
+
+  def Show::lowest_rating
+    Show.where("rating < Show::highest_rating").last
+  end
 end
